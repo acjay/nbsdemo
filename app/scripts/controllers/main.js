277bbf11dfbfcccc3129a5bd2b13f29ec096583a
Alan <mmfunkyd@hotmail.com>
2014-01-31 21:09:20 -0500

    initial commit, working grunt

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('nbsdemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
